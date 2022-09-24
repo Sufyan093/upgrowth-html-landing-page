ac4f9184be9e5a2910644b3ebb8149d74e804470
sufian <skontech@hotmail.com>
2022-09-25 01:42:12 +0500

    HTML landing page

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+tailwind.config = {
+    theme: {
+        screens: {
+            'xxs': '320px',
+            // => @media (min-width: 320px) { ... }
+
+            'xs': '375px',
+            // => @media (min-width: 375px) { ... }
+
+            'sm': '425px',
+            // => @media (min-width: 425px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'white': '#ffffff',
+            'brand-color': '#53A89A',
+            'circle-one': '#5264BE',
+            'circle-two': '#788DF5',
+            'circle-three': '#CBD2FB',
+            'circle-four': '#E9E9E9',
+            'circle-five': '#F08B34',
+            'third-section': '#1F6459',
+        },
+        extend: {},
+    },
+    plugins: []
+}
